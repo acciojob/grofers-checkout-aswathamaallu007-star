704ae98acaa2d7dd575c204a4004426e1ae3a3d2
aswathamaallu007-star <aswathamaallu786@gmail.com>
2025-09-10 03:43:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,21 +4,20 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-	const table = document.getElementById("table")
-  let totalsum = 0
-  const sum = document.querySelectorAll(".price")
+    const table = document.getElementById("table")
+	let sum = 0
+	const prices = document.querySelectorAll(".prices")
 	function total(){
-		for(let ch of sum){
-			totalsum += parseInt(ch.innerText)
+		for(let ch of prices){
+			sum += parseInt(ch.innerText)
 		}
 		const newrow = document.createElement("tr")
 		const newdata = document.createElement("td")
 		newdata.id = "ans"
 		newdata.colSpan = 2
-		newdata.innerText = `Total Amount : ${totalsum}`
-		newrow.appendChild(newdata) 
+		newdata.innerText = `Total Price is ${sum}`
+		newrow.appendChild(newdata)
 		table.appendChild(newrow)
-		
 	}
 	total()
 };
